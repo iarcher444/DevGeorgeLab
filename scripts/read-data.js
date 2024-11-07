a250e4486b9486c73d69b58f9c65a9f074e888d4
Isabella Archer <167831284+iarcher444@users.noreply.github.com>
2024-11-07 11:48:22 -0600

    Update read-data.js

--- scripts/read-data.js
+++ scripts/read-data.js
@@ -1,19 +1,30 @@
-document.getElementById('fetchButton').addEventListener('click', function () {
-    fetch('https://randomuser.me/api/')
-        .then(response => response.json())
-        .then(data => {
-            const user = data.results[0];
-            document.getElementById('dataDisplay').innerHTML = `
-                <h3>User Information</h3>
-                <p><strong>Name:</strong> ${user.name.first} ${user.name.last}</p>
-                <p><strong>Email:</strong> ${user.email}</p>
-                <p><strong>Location:</strong> ${user.location.city}, ${user.location.country}</p>
-                <img src="${user.picture.large}" alt="User Picture" class="img-fluid rounded-circle">
-            `;
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            document.getElementById('dataDisplay').innerHTML = `<p>Error fetching data.</p>`;
-        });
-});
 
+
+// $.ajax({
+//   dataType: "json",
+//   url: url,
+//   data: data,
+//   success: success
+// });
+
+
+$("button").click(function(){
+    $.getJSON("https://barrycumbie.github.io/barrycumbie/data.json", function(result){
+      $.each(result, function(i, field){
+        $("div").append(field + " ");
+      });
+    });
+  });
+
+// $.getJSON( "https://barrycumbie.github.io/barrycumbie/data.js", function( data ) {
+//     console.log(data); 
+//     var items = [];
+//     $.each( data, function( key, val ) {
+//       items.push( "<li id='" + key + "'>" + val + "</li>" );
+//     });
+   
+//     $( "<ul/>", {
+//       "class": "my-new-list",
+//       html: items.join( "" )
+//     }).appendTo( "body" );
+//   });
