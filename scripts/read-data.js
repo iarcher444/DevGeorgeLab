194b466f0169f0657e1e0cf131360b9bc821c681
Isabella Archer <167831284+iarcher444@users.noreply.github.com>
2024-11-07 14:04:41 -0600

    Update read-data.js

--- scripts/read-data.js
+++ scripts/read-data.js
@@ -1,5 +1,5 @@
 $("button").click(function(){
-    $.getJSON("https://barrycumbie.github.io/barrycumbie/data.json", function(result){
+    $.getJSON("https://iarcher444.github.io/DevGeorgeLab/data.json", function(result){
       $.each(result, function(i, field){
         $("div").append(field + " ");
       });
